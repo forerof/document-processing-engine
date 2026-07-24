568adb0c013c2dfdd5155f99026e86fad6c3aacd
forerof <jyanezf@unal.edu.co>
2026-07-24 17:44:28 -0500

    refactor: remove unused test function for clearing processed files repository. pmv_1: processing engine

--- src/main.js
+++ src/main.js
@@ -3,12 +3,3 @@ function helloWorld() {
   var name = "Smith";
   Logger.log(name);
 }
-
-/**
- * Elimina todos los registros del repositorio de procesados.
- */
-function testElectronicIndexService_ClearRepository() {
-  ProcessedFilesRepository.clear();
-
-  Logger.log("Repositorio limpiado correctamente.");
-}
